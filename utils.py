fbbb30d5e4d37be524ab532e7044407c3b4174b5
markmipt <markmipt@localhost>
2016-05-24 17:00:51 +0300

    this version almost work

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from ConfigParser import RawConfigParser
 import sys
 sys.path.append('/usr/share/mmass')
 from sys import argv
+import numpy as np
 from mspy import parser_mzml
 from time import time
 import os
@@ -21,6 +22,47 @@ class CustomRawConfigParser(RawConfigParser):
             return val[::-1].split('|', 1)[0][::-1]
         else:
             return ''
+
+def find_local_max_new(RTs, Is):
+    maximums = []
+    lv = RTs.searchsorted(RTs - 1.5, side='left')
+    rv = RTs.searchsorted(RTs + 1.5, side='right')
+    for idx in range(len(RTs)):
+        # lv = RTs.searchsorted(rt - 1.5, side='left')
+        # rv = RTs.searchsorted(rt + 1.5, side='right')
+        if Is[idx] == max(Is[lv[idx]:rv[idx]]) and lv[idx] != rv[idx] - 1 and RTs[rv[idx] - 1] - RTs[lv[idx]] >= 0.2:
+            maximums.append(idx)
+    return maximums
+
+
+
+def find_peaks(q, q_out):
+    for rec in iter(q.get, None):
+        v, x, y, mzst_t, chs_t, pis_t, pI_t = rec
+        idt = x.argsort()
+        x = x[idt]
+        y = y[idt]
+        mzst_t = mzst_t[idt]
+        chs_t = chs_t[idt]
+        pis_t = pis_t[idt]
+        pI_t = pI_t[idt]
+        temp_idx = np.array([True for _ in x], dtype=bool)
+        x = x[temp_idx]
+        y = y[temp_idx]
+        mzst_t = mzst_t[temp_idx]
+        chs_t = chs_t[temp_idx]
+        pis_t = pis_t[temp_idx]
+        pI_t = pI_t[temp_idx]
+        temp_idx = find_local_max_new(x, y)
+        x_new2 = x[temp_idx]
+        y_new2 = y[temp_idx]
+        mzst_t = mzst_t[temp_idx]
+        chs_t = chs_t[temp_idx]
+        pis_t = pis_t[temp_idx]
+        pI_t = pI_t[temp_idx]
+        q_out.put(zip(mzst_t, x_new2, y_new2, chs_t, pis_t, pI_t))
+    q_out.put(None)
+
 
 def iterate_spectra(fname, min_ch, max_ch, min_i):
     MZML_data = parser_mzml.parseMZML(fname)
@@ -42,8 +84,8 @@ def iterate_spectra(fname, min_ch, max_ch, min_i):
                 peaks.remisotopes()
                 for peak in peaks:
                     if peak.charge and min_ch <= peak.charge <= max_ch and peak.intensity >= min_i:
-                        neutral = peak.mz * peak.charge - 1.0073 * peak.charge
-                        q_output.put((neutral, RT, peak.intensity, peak.charge))
+                        # neutral = peak.mz * peak.charge - 1.0073 * peak.charge
+                        q_output.put((peak.mz, RT, peak.intensity, peak.charge))
         q_output.put(None)
 
     for i in range(nprocs):
@@ -55,7 +97,7 @@ def iterate_spectra(fname, min_ch, max_ch, min_i):
     idx = 1
     ms1_idx = 1
 
-    while 1:
+    while 1:#ms1_idx <= 2000:
         sc = MZML_data.scan(idx)
         if sc == False:
             print 'total number of spectra = %d\ntotal number of ms1 spectra = %d' % (idx - 1, ms1_idx - 1)
@@ -69,50 +111,96 @@ def iterate_spectra(fname, min_ch, max_ch, min_i):
         q.put(None)
 
     del MZML_data
-
+    results = []
     while nprocs > 0:
         for rec in iter(q_output.get, None):
-            yield rec
+            results.append(rec)
+            # yield rec
         nprocs -= 1
 
     for p in procs:
         p.terminate()
 
+    peak_id = -1
+    number_of_peaks = len(results)
 
-def iterate_spectra_BU(fname, min_ch, max_ch):
-    # filename = os.path.splitext(fname)[0]
-    MZML_data = parser_mzml.parseMZML(fname)
-    print 'parsing was started'
-    MZML_data.load()
-    print 'parsing was finished'
-    # output_csv = open('%s_peaks.csv' % (filename,), 'w')
-    idx = 1
-    ms1_idx = 1
+    mzst = np.empty(number_of_peaks, dtype=np.dtype('f4'))
+    mzs = np.empty(number_of_peaks, dtype=np.dtype('f4'))
+    RTs = np.empty(number_of_peaks, dtype=np.dtype('f2'))
+    Is = np.empty(number_of_peaks, dtype=int)
+    chs = np.empty(number_of_peaks, dtype=np.dtype('i2'))
+    pis = np.empty(number_of_peaks, dtype=np.dtype('i4'))
+    pIs = np.empty(number_of_peaks, dtype=np.dtype('f4'))
 
-    while 1:
-        sc = MZML_data.scan(idx)
-        if sc == False:
-            print 'total number of spectra = %d\ntotal number of ms1 spectra = %d' % (idx - 1, ms1_idx - 1)
-            break
-        if sc.msLevel == 1:
-            peaks = sc.peaklist
-            if len(peaks) and peaks[-1].mz - peaks[0].mz >= 600:
-                if ms1_idx % 1000 == 0:
-                    print '%d of ms1 spectra were processed' % (ms1_idx, )
+    for rec in results:
+        if 1:#try:
+            mz = float(rec[0])
+            RT = float(rec[1])
+            I = float(rec[2])
+            cz = int(rec[3])
+            try:
+                pI = float(rec[6])
+            except:
+                pI = 7
+            neutral = mz * cz - 1.0073 * cz
+            peak_id += 1
+            if 1:#2 < RT < 100:
+                mzst[peak_id] = neutral
+                mzs[peak_id] = mz
+                RTs[peak_id] = RT
+                Is[peak_id] = I
+                chs[peak_id] = cz
+                pis[peak_id] = peak_id
+                pIs[peak_id] = pI
+    print len(mzs), 'len(mzs)'
+    print peak_id, 'peak_id'
+    idx = np.argsort(mzs)
+    mzs = mzs[idx]
+    mzst = mzst[idx]
+    RTs = RTs[idx]
+    Is = Is[idx]
+    chs = chs[idx]
+    pis = pis[idx]
+    pIs = pIs[idx]
+    minv = -1
 
-                RT = sc.retentionTime / 60
-                peaks = sc.peaklist
-                peaks.deisotope(maxCharge=max_ch, mzTolerance=0.01, intTolerance=0.5)
-                peaks.remisotopes()
-                for peak in peaks:
-                    if peak.charge and min_ch <= peak.charge <= max_ch:
-                        neutral = peak.mz * peak.charge - 1.0073 * peak.charge
-                        # output_csv.write('%.5f\t%.4f\t%d\t%d\n' % (peak.mz, RT, peak.intensity, peak.charge))
-                        yield (neutral, RT, peak.intensity, peak.charge)
 
-                ms1_idx += 1
-        idx += 1
-    # output_csv.close()
+
+    procs = []
+    nprocs = 12
+    q = Queue()
+    q_out = Queue()
+
+    for i in range(nprocs):
+        p = Process(target=find_peaks, args=(q, q_out))
+        procs.append(p)
+        p.start()
+
+    lv = mzs.searchsorted(mzs - 0.01, side='left')
+    rv = mzs.searchsorted(mzs + 0.01, side='right')
+    for idx in range(len(mzs)):
+        if mzs[idx] >= minv:
+            minv = mzs[idx] + 0.005
+            if mzs[lv[idx]:rv[idx]].size > 5 and RTs[lv[idx]:rv[idx]].max() - RTs[lv[idx]:rv[idx]].min() > 0.25:
+                q.put((mzs[idx], RTs[lv[idx]:rv[idx]], Is[lv[idx]:rv[idx]], mzst[lv[idx]:rv[idx]], chs[lv[idx]:rv[idx]], pis[lv[idx]:rv[idx]], pIs[lv[idx]:rv[idx]]))
+                # q.put((mzst[idx], RTs[lv[idx]:rv[idx]], Is[lv[idx]:rv[idx]], chs[lv[idx]:rv[idx]], pis[lv[idx]:rv[idx]], pIs[lv[idx]:rv[idx]]))
+    for p in procs:
+        q.put(None)
+
+    outres = set()
+    while nprocs > 0:
+        for rec in iter(q_out.get, None):
+            outres.update(rec)
+        nprocs -= 1
+
+    for p in procs:
+        p.terminate()
+
+    print 'Total number of peaks = %d' % (len(mzs), )
+#        self.data = list(set(self.data))
+    print 'Total number of unique peaks = %d' % (len(outres), )
+    for res in outres:
+        yield res
 
 
 def peptide_gen(settings):
@@ -148,7 +236,7 @@ def prot_gen(settings):
 
 seen_target = set()
 seen_decoy = set()
-def prot_peptides(prot_seq, enzyme, mc, minlen, maxlen, is_decoy):
+def prot_peptides(prot_seq, enzyme, mc, minlen, maxlen, is_decoy, dont_use_seen_peptides=False):
 
     dont_use_fast_valid = parser.fast_valid(prot_seq)
     peptides = parser.cleave(prot_seq, enzyme, mc)
@@ -164,15 +252,18 @@ def prot_peptides(prot_seq, enzyme, mc, minlen, maxlen, is_decoy):
                 if prot_seq[0] == 'M' and prot_seq.startswith(pep):
                     if minlen <= plen - 1 <= maxlen:
                         forms.append(pep[1:])
-                    if minlen <= plen - 2 <= maxlen:
-                        forms.append(pep[2:])
+                    # if minlen <= plen - 2 <= maxlen:
+                    #     forms.append(pep[2:])
             for f in forms:
-                if f not in seen_target and f not in seen_decoy:
-                    if is_decoy:
-                        seen_decoy.add(f)
-                    else:
-                        seen_target.add(f)
+                if dont_use_seen_peptides:
                     yield f
+                else:
+                    if f not in seen_target and f not in seen_decoy:
+                        if is_decoy:
+                            seen_decoy.add(f)
+                        else:
+                            seen_target.add(f)
+                        yield f
 
 def get_prot_pept_map(settings):
     seen_target.clear()
@@ -180,6 +271,7 @@ def get_prot_pept_map(settings):
 
     prefix = settings.get('input', 'decoy prefix')
     enzyme = get_enzyme(settings.get('search', 'enzyme'))
+    print enzyme
     mc = settings.getint('search', 'number of missed cleavages')
     minlen = settings.getint('search', 'peptide minimum length')
     maxlen = settings.getint('search', 'peptide maximum length')
@@ -189,7 +281,9 @@ def get_prot_pept_map(settings):
 
     for desc, prot in prot_gen(settings):
         dbinfo = desc.split(' ')[0]
-        for pep in prot_peptides(prot, get_enzyme(enzyme), mc, minlen, maxlen, desc.startswith(prefix)):
+        for pep in prot_peptides(prot, enzyme, mc, minlen, maxlen, desc.startswith(prefix), dont_use_seen_peptides=True):
+            if dbinfo == 'sp|P02787|TRFE_HUMAN':
+                print pep
             pept_prot.setdefault(pep, []).append(dbinfo)
             protsN.setdefault(dbinfo, set()).add(pep)
     for k, v in protsN.items():
