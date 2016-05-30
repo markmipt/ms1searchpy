712808623d0371e443066d6ba77026fa85b9df39
Mark Ivanov <markmipt@users.noreply.github.com>
2016-05-30 19:03:16 +0300

    delete local names from source files

--- search.py
+++ search.py
@@ -1,9 +1,7 @@
 from sys import argv
-import sys
-sys.path.insert(0, '/home/mark/work/PycharmProjects/ms1searchpy/')
 import main
 
-inputfile = argv[1]#'/home/mark/work/PycharmProjects/ms1searchpy/tests/s6.mzML'
+inputfile = argv[1]
 settings = main.settings(argv[2 ])
 
 main.process_file(inputfile, settings)
