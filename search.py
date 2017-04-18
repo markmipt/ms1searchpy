e28758ae0a0116cc39074ae0befa174428d6d254
markmipt <markmipt@localhost>
2017-04-18 18:37:06 +0300

    typo fix

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ from sys import argv
 import main, utils
 
 inputfile = argv[1]
-settings = utils.settings(argv[2 ])
+settings = utils.settings(argv[2])
 
 main.process_file(inputfile, settings)
 print 'The search is finished.'
