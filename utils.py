20387e29580a9477ea30bc0fea62e2c4ede600cc
Mark Ivanov <markmipt@gmail.com>
2017-07-18 17:24:55 +0300

    Add tuning for Dinosaur. Add threshold for number of scans for peptide features. Add writing Intensity to output

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ import csv
 import subprocess
 
 
-def iterate_spectra(fname, min_ch, max_ch, min_isotopes):
+def iterate_spectra(fname, min_ch, max_ch, min_isotopes, min_scans):
     if os.path.splitext(fname)[-1].lower() == '.mzml':
-        subprocess.call(['java', '-Djava.awt.headless=true', '-jar', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Dinosaur/Dinosaur-1.1.3.free.jar'), fname])
+        subprocess.call(['java', '-Djava.awt.headless=true', '-jar', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Dinosaur/Dinosaur-1.1.3.free.jar'), '--advParams=Dinosaur/adv.txt', fname])
         fname = os.path.splitext(fname)[0] + '.features.tsv'
     with open(fname, 'rb') as infile:
         csvreader = csv.reader(infile, delimiter='\t')
@@ -16,6 +16,7 @@ def iterate_spectra(fname, min_ch, max_ch, min_isotopes):
         RT_ind = header.index('rtApex')
         ch_ind = header.index('charge')
         nIsotopes_ind = header.index('nIsotopes')
+        Int_ind = header.index('intensityApex')
         nScans_ind = header.index('nScans')
         idx = 0
         for z in csvreader:
@@ -24,9 +25,10 @@ def iterate_spectra(fname, min_ch, max_ch, min_isotopes):
             ch = float(z[ch_ind])
             nIsotopes = float(z[nIsotopes_ind])
             nScans = int(z[nScans_ind])
+            I = float(z[Int_ind])
             idx += 1
-            if nIsotopes >= min_isotopes and min_ch <= ch <= max_ch:
-                yield nm, RT, ch, idx
+            if nIsotopes >= min_isotopes and min_ch <= ch <= max_ch and nScans >= min_scans:
+                yield nm, RT, ch, idx, I
 
 
 def peptide_gen(args):
